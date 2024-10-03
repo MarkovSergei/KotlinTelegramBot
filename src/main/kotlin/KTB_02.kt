2267ab5a1f95b23a2d13405e17bf283884b9a3b9
MarkovSergei <dzey1@bk.ru>
2024-10-03 23:04:44 +0800

    KTB-04

--- src/main/kotlin/KTB_02.kt
+++ src/main/kotlin/KTB_02.kt
@@ -1,12 +1,19 @@
 package org.example
-
 import java.io.File
 
 fun main() {
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    val lines = wordsFile.readLines()
+    val dictionary = mutableListOf<Word>()
+    val lines: List<String> = wordsFile.readLines()
     for (line in lines) {
-        println(line)
+        val lineParts = line.split("|")
+        val word = Word(
+            original = lineParts[0],
+            translate = lineParts[1],
+            correctAnswersCount = lineParts.getOrNull(2) ?: 0
+        )
+        dictionary.add(word)
     }
+    println(dictionary)
 }
