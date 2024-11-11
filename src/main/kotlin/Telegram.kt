0b8f91c6695bcb49c4a7ca435be4475d5624a426
MarkovSergei <dzey1@bk.ru>
2024-11-11 23:27:48 +0800

    KTB-16

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val botToken = args[0]
-    var updateId = 0
+    var updateId: Long = 0
     val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
     val updateIdRegex: Regex = "\"update_id\":(.+?),".toRegex()
     val chatIdRegex: Regex = "\"chat\":\\{\"id\":(\\d+),".toRegex()
@@ -13,12 +13,12 @@ fun main(args: Array<String>) {
     while (true) {
         Thread.sleep(2000)
         val updates: String = telegramBotService.getUpdates(updateId)
-        println(updates)
-        val updateIdMatch = updateIdRegex.find(updates)?.groups?.get(1)?.value?.toInt() ?: continue
+        //println(updates)
+        val updateIdMatch = updateIdRegex.find(updates)?.groups?.get(1)?.value?.toLong() ?: continue
         updateId = updateIdMatch + 1
         val text = messageTextRegex.find(updates)?.groups?.get(1)?.value
         val data = dataRegex.find(updates)?.groups?.get(1)?.value
-        val chatId = chatIdRegex.find(updates)?.groups?.get(1)?.value?.toInt() ?: continue
+        val chatId = chatIdRegex.find(updates)?.groups?.get(1)?.value?.toLong() ?: continue
         println(text)
 
         if (text == "Hello") telegramBotService.sendMessage(chatId, "Hello")
