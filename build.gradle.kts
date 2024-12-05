2f31d1a039a0895da5af6c3d78a56e8e34550d5b
MarkovSergei <dzey1@bk.ru>
2024-12-05 16:34:15 +0800

    KTB-23

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
@@ -22,7 +22,7 @@ tasks.test {
 }
 
 application {
-    mainClass.set { "Telegram.kt" }
+    mainClass.set ( "TelegramKt" )
 }
 
 kotlin {
