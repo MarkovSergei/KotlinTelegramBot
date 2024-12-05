0674b653f45b70a0969f996ad2caaaf6cc72287e
MarkovSergei <dzey1@bk.ru>
2024-12-05 15:34:42 +0800

    KTB-23

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    application
 }
 
 group = "org.example"
@@ -18,6 +20,12 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+application {
+    mainClass.set { "Telegram.kt" }
+}
+
 kotlin {
     jvmToolchain(20)
 }
+
